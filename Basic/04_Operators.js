776f95dd047a61cc7db65f6b0cf56fe3e9d56882
Farukh Khan <118312333+Farukh2000@users.noreply.github.com>
2026-05-10 09:28:09 +0000

    Operations

---
+++ Basic/04_Operators.js
@@ -0,0 +1,8 @@
+console.log('2fnf'>1); // we can not compare two different data types.
+
+console.log('b'>'a');   // it compare according to ASCII value.
+
+
+console.log('1'==1) // check
+console.log('1'===1) // strictly check
+
