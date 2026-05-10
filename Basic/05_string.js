d1a4988688a44186d4674c2382d63a8e9d79c827
Farukh Khan <118312333+Farukh2000@users.noreply.github.com>
2026-05-10 15:55:15 +0000

    String methods

---
+++ Basic/05_string.js
@@ -0,0 +1,29 @@
+console.log(`hi my name is farukh and i am ${20} year old`);
+
+
+const str=new String("farukh");  // another way to declare a string
+
+console.log(str);
+
+// methods of string
+
+let a="hello"
+
+console.log(a.charAt(1));
+console.log(a.indexOf('l'));
+
+let str1=" banana shek "
+console.log(str1.length);
+
+// substring,slice(we can use -ve value in slice)
+let newStr=str1.substring(0,5);
+console.log(newStr);
+
+// trim method (remove starting and last spaces)
+console.log(str1.trim().length);
+
+console.log(str1.split());
+
+
+
+
