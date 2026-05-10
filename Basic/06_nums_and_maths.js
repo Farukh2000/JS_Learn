ec967d21dcbe6915641b81fdaaa85ca921bdb39c
Farukh Khan <118312333+Farukh2000@users.noreply.github.com>
2026-05-10 16:14:17 +0000

    nums method

---
+++ Basic/06_nums_and_maths.js
@@ -0,0 +1,20 @@
+const bal= new Number(100);
+console.log(bal.toString().length);
+
+// tofixed
+console.log(bal.toFixed(3)); //
+
+const a=123.83434
+
+console.log(a.toPrecision(3)); 
+
+
+let b=100000000000
+
+console.log(b.toLocaleString('en-IN'));
+
+
+
+
+
+
