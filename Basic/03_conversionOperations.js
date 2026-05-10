58ddfd0c6d42b9e9c3c26adb0baa7e4471d3043b
Farukh Khan <118312333+Farukh2000@users.noreply.github.com>
2026-05-10 09:18:11 +0000

    conversionOPr

--- Basic/03_conversionOperations.js
+++ Basic/03_conversionOperations.js
@@ -14,3 +14,11 @@ console.log("when we use Num+str  "+Number(c))
 console.log("Original val "+typeof b)
 
 console.log("Number conversion "+ typeof Number(b))
+
+console.log('1'+2)
+console.log('1'+'2')
+console.log(1+'2')
+
+let num1,num2,num3=1; // it will assign only for num3
+
+console.log(num2)
