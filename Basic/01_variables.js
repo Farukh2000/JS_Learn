958b46344ab1fb4a5bcb846c8ea460d5c8295604
Farukh Khan <118312333+Farukh2000@users.noreply.github.com>
2026-05-10 08:48:50 +0000

    variables

--- Basic/01_variables.js
+++ Basic/01_variables.js
@@ -2,7 +2,11 @@ const accID=7897284;
 let accEmail="xyz@gmail.com"
 var accPswd="ada@123"
 accCity="jewar"
+accEmail="abc@gmail.com"
 
 //accId=78242312312;
 
 console.log(accEmail)
+
+console.table([accID,accEmail,accPswd,accCity]);
+
