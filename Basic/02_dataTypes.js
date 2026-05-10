c23707c0f7f64075235951ea1c0c8f80e36af367
Farukh Khan <118312333+Farukh2000@users.noreply.github.com>
2026-05-10 08:47:32 +0000

    data types

---
+++ Basic/02_dataTypes.js
@@ -0,0 +1,10 @@
+// a=5
+// b='h'
+// c='hello'
+// d=1.3
+// e=1
+// console.log(typeof(a))
+// console.log(typeof(b))
+// console.log(typeof(c))
+// console.log(typeof(d))
+// console.log(typeof(e))
