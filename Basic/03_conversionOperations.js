4c42ffae7a0737c55123128638f5e3c1d5cf2396
Farukh Khan <118312333+Farukh2000@users.noreply.github.com>
2026-05-10 09:11:43 +0000

    conversionOP

---
+++ Basic/03_conversionOperations.js
@@ -0,0 +1,16 @@
+let score=30;
+
+console.log(typeof String(score))
+
+let b="123"
+let c="123hd"
+let d=true; // return 1 in a number  and return true in string.
+let e=false; // return 0 in a number.
+console.log(String(d))
+console.log(Number(d))
+console.log(Number(e))
+console.log("when we use Num+str  "+Number(c))
+
+console.log("Original val "+typeof b)
+
+console.log("Number conversion "+ typeof Number(b))
